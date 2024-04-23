69d5f74548f6f8876d065d650471896ecd47e2ca
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2024-04-23 17:02:16 +0200

    Agrega nuevas rutas y funciones en el servidor

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 1 - source venv/bin/activate 
 2 - uvicorn main:app --reloa
 3 - deactivate
+
+
+uvicorn main:app --reload
+uvicorn main:app
+uvicorn main:app --host 0.0.0.0 --port 8000
--- serveur/main.py
+++ serveur/main.py
@@ -5,8 +5,21 @@ from fastapi import FastAPI, APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from database import *
 from handle_bd_function import *
+
+
+class Item(BaseModel):
+    nom: str
+    ville: str
+    active: bool
+    frequence: int
+    temperature: float
+    humidite: float
+    pluviosite: float
+
 
 app = FastAPI()
 
@@ -42,12 +55,14 @@ def get_all_data():
     result = selectAllStations()
     return result
 
-
-
+'''
+    I have get a json object from the client side, and I want to insert it in the database
+'''
 @router.post("/send_data")
-def send_data(request: Request, nom: str = Form(...), ville: str = Form(...), active: bool = Form(...), frequence: int = Form(...), temperature: float = Form(...), humidite: float = Form(...), pluviosite: float = Form(...) ):
-   
-    result = insertStation(nom, ville, active, frequence, temperature, humidite, pluviosite )
+
+async def send_data(data: Item):
+    data = data.model_dump()
+    result = insertStation(data.nom, data.ville, data.active, data.frequence, data.temperature, data.humidite, data.pluviosite )
     
     if result:
     
